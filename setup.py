a7811d2f30df06f18924e2b56fc1fcae25e5fdf4
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2023-04-24 10:34:11 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="jupyterhub-backendspawner",
+    version="0.1.0",
+    description="JupyterHub Spawner to spawn on different systems.",
+    url="https://github.com/kreuzert/jupyterhub-backendspawner",
+    author="Tim Kreuzer",
+    author_email="t.kreuzer@fz-juelich.de",
+    license="3-BSD",
+    packages=find_packages(),
+    install_requires=["jupyterhub","traitlets"],
+    python_requires=">=3.9",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
