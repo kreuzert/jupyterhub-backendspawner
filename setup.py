7732073df9ca313207e8adcaac695fe1baa5a702
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2023-04-26 16:44:12 +0200

    version 0.1.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="jupyterhub-backendspawner",
-    version="0.1.6",
+    version="0.1.7",
     description="JupyterHub Spawner to spawn on different systems.",
     url="https://github.com/kreuzert/jupyterhub-backendspawner",
     author="Tim Kreuzer",
